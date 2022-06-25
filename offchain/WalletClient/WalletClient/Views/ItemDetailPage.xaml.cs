64f7e10d0007745ba349a756d83aee82bc104ce6
Sakul Jaruthanaset <au.perspectives@gmail.com>
2022-06-25 10:01:57 +0700

    Initialize client & server for shopping online website.

---
+++ offchain/WalletClient/WalletClient/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WalletClient.ViewModels;
+using Xamarin.Forms;
+
+namespace WalletClient.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ offchain/WalletClient/WalletClient/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using WalletClient.Models;
+using WalletClient.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WalletClient.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
