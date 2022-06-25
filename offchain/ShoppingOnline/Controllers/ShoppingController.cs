2c7104f66a2d818ca86e568df0734dad9c378f7d
Sakul Jaruthanaset <au.perspectives@gmail.com>
2022-06-25 14:57:32 +0700

    Mockup products.

--- offchain/ShoppingOnline/Controllers/ShoppingController.cs
+++ offchain/ShoppingOnline/Controllers/ShoppingController.cs
@@ -11,17 +11,31 @@ namespace ShoppingOnline.Controllers
     {
         private readonly IPointService pointService;
 
+        // HACK: TEMPORARY PRODUCTS
+        private static IEnumerable<KeyValuePair<string, int>> products = new List<KeyValuePair<string, int>>
+        {
+            new KeyValuePair<string, int>("P01", 10),
+            new KeyValuePair<string, int>("P02", 3),
+            new KeyValuePair<string, int>("P03", 5),
+        };
+
         public ShoppingController(IPointService pointService)
         {
             this.pointService = pointService;
         }
 
         [HttpPost]
-        public async Task<TrackingResponse> Buy([FromBody] BuyRequest request)
+        public async Task<ActionResult<TrackingResponse>> Buy([FromBody] BuyRequest request)
         {
+            if (products.Any(it => it.Key == request.ProductId))
+            {
+                return BadRequest("Product not found");
+            }
+            var selectedProduct = products.FirstOrDefault(it => it.Key == request.ProductId);
+            var productPrice = selectedProduct.Value;
+
             var trackingId = Guid.NewGuid().ToString();
             track.AddBuyTracking(trackingId, request);
-            var productPrice = 1;
             await pointService.Buy(request.ProductId, request.WalletAddress, productPrice);
             track.UpdateBalance(request.WalletAddress, -productPrice);
             return new TrackingResponse(request)
